220c5af0be5517b49e5664a948a10553ffe69c87
Leonardo-Corazzini <leocora10@gmail.com>
2025-03-02 03:43:38 +0100

    add 2 bonus

--- script.js
+++ script.js
@@ -44,20 +44,45 @@
 // Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
 // 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
 // Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".
-function lanciaDado() {
-    return promessa = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const incastro = Math.floor(Math.random() * (5 - 1 + 1)) + 1
-            const numero = Math.floor(Math.random() * (6 - 1 + 1)) + 1
-            incastro !== 1 ? resolve(numero) : reject('dado incastrato')
+// function lanciaDado() {
+//     return promessa = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             const incastro = Math.floor(Math.random() * (5 - 1 + 1)) + 1
+//             const numero = Math.floor(Math.random() * (6 - 1 + 1)) + 1
+//             incastro !== 1 ? resolve(numero) : reject('dado incastrato')
 
 
-        }, 3000);
-    })
+//         }, 3000);
+//     })
+// }
+
+
+// lanciaDado()
+//     .then(num => console.log(num))
+//     .catch(err => console.error(err))
+//     .catch(err => console.error(err))
+
+
+
+function creaLanciaDado() {
+    let ultimoNumero
+    return function lanciaDado() {
+        return promessa = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const incastro = Math.floor(Math.random() * (5 - 1 + 1)) + 1
+                const numero = Math.floor(Math.random() * (6 - 1 + 1)) + 1
+                incastro !== 1 ? resolve(numero) : reject('dado incastrato')
+                ultimoNumero === numero && console.log(`incredibile numero di prima ${ultimoNumero} nunero di ora ${numero}`)
+                ultimoNumero = numero
+
+
+
+            }, 3000);
+        })
+    }
 }
 
-
-lanciaDado()
+const tiro = creaLanciaDado()
+tiro()
     .then(num => console.log(num))
-    .catch(err => console.error(err))
-    .catch(err => console.error(err))
+    .catch(err => console.err(err))
